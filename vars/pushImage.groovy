d639e7d875081099475fc4267ee224d801eedd57
Mohamedmagdy220 <mohamedelngar062@gmail.com>
2025-07-15 16:27:59 +0300

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,5 +1,7 @@
 def call() {
     def image = "${env.IMAGE_NAME}:${env.IMAGE_TAG}"
     echo "📤 Pushing Docker image: ${image}"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
     sh "docker push ${image}"
 }
